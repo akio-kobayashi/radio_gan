f2d2b25fb258b12f1460675967c575aeaf95ed39
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2025-01-22 09:59:13 +0900

    modified sources

--- bin/compute_features.py
+++ bin/compute_features.py
@@ -10,39 +10,30 @@ import warnings
 warnings.filterwarnings('ignore')
 
 '''
-    input_csv format:  key, source, speaker, 
-    output_csv format: key, source, melspec, speaker, hearing
+    input_csv format:  key, clean, noisy, 
+    output_csv format: key, clean, noisy
 '''
-def compute_features(input_csv, output_csv, output_dir, output_speaker):
-    keys, wavs, specs, speakers, hearing = [], [], [], [], []
+def compute_features(input_csv, output_csv, output_dir):
+    keys, cleans, noisys = [], [], []
 
-    speaker2id={}
-    number = 1 # start from 1 because <UNK> should be assigned to 0
     df = pd.read_csv(input_csv)
     for idx, row in df.iterrows():
-        melspec = M.get_mel_spectrogram(row['source'])
-        output_path = os.path.join(output_dir, row['key']) + '.pt'
-        torch.save(melspec, output_path)
+        clean = M.get_mel_spectrogram(row['clean'])
+        clean_path = os.path.join(output_dir, row['key']+'_clean') + '.pt'
+        torch.save(clean, clean_path)
+
+        noisy = M.get_mel_spectrogram(row['noisy'])
+        noisy_path = os.path.join(output_dir, row['key']+'_noisy') + '.pt'
+        torch.save(noisy, noisy_path)
+        
         keys.append(row['key'])
-        wavs.append(row['source'])
-        specs.append(output_path)
-        speakers.append(row['speaker'])
-        if row['speaker'] not in speaker2id:
-            speaker2id[row['speaker']] = number
-            number += 1
-        if row['speaker'].startswith('B'):
-            hearing.append('NH')
-        else:
-            hearing.append('DF')
+        clean.append(clean_path)
+        noisy.append(noisy_path)
     
     out_df = pd.DataFrame(index=None)
-    out_df['key'], out_df['source'] = keys, wavs
-    out_df['melspec'], out_df['speaker'], out_df['hearing'] = specs, speakers, hearing
+    out_df['key'], out_df['clean'], out_df['noisy'] = keys, cleans, noisys
 
     out_df.to_csv(output_csv, index=False)
-    with open(output_speaker, 'w') as f:
-        for speaker, id in speaker2id.items():
-            f.write(f'{speaker} {id}\n')
 
 def compute_mean_var(input_csv):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
@@ -52,7 +43,11 @@ def compute_mean_var(input_csv):
     spec_sum=0.
     spec_sum_square=0.
     for idx, row in df.iterrows():
-        melspec = torch.load(row['melspec']).to(device)
+        melspec = torch.load(row['clean']).to(device)
+        total_frames += melspec.shape[-1]
+        spec_sum += torch.sum(melspec, dim=-1)
+        spec_sum_square += torch.sum(torch.square(melspec))
+        melspec = torch.load(row['noisy']).to(device)
         total_frames += melspec.shape[-1]
         spec_sum += torch.sum(melspec, dim=-1)
         spec_sum_square += torch.sum(torch.square(melspec))
@@ -94,10 +89,9 @@ if __name__ == '__main__':
     parser.add_argument('--output_csv', type=str, required=True)
     parser.add_argument('--output_dir', type=str, default='./')
     parser.add_argument('--output_stats', type=str, default='stats.npz')
-    parser.add_argument('--output_speaker', type=str)
     args=parser.parse_args()
        
-    compute_features(args.input_csv, args.output_csv, args.output_dir, args.output_speaker)
+    compute_features(args.input_csv, args.output_csv, args.output_dir)
     mean, var = compute_mean_var(args.output_csv)
     save_mean_var(mean, var, args.output_stats)
     
